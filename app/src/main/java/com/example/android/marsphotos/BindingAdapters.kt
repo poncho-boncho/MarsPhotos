c4aab33fc8f8ef1b9169497c1efd9beff70f5399
poncho_boncho <Sorkolas@mail.ru>
2023-05-27 19:49:02 +0300

    added error handling

--- app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
+++ app/src/main/java/com/example/android/marsphotos/BindingAdapters.kt
@@ -1,12 +1,14 @@
 package com.example.android.marsphotos
 
 import android.net.Uri
+import android.view.View
 import android.widget.ImageView
 import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
 import com.example.android.marsphotos.network.MarsPhoto
+import com.example.android.marsphotos.overview.MarsApiStatus
 import com.example.android.marsphotos.overview.PhotoGridAdapter
 
 @BindingAdapter("imageUrl")
@@ -25,4 +27,23 @@ fun bindRecyclerView(recyclerView: RecyclerView,
     data: List<MarsPhoto>?){
     val adapter = recyclerView.adapter as PhotoGridAdapter
     adapter.submitList(data)
+}
+
+@BindingAdapter("marsApiStatus")
+fun bindStatus(statusImageView: ImageView,
+        status: MarsApiStatus?){
+    when (status){
+        MarsApiStatus.LOADING -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.loading_animation)
+        }
+        MarsApiStatus.ERROR -> {
+            statusImageView.visibility = View.VISIBLE
+            statusImageView.setImageResource(R.drawable.ic_connection_error)
+        }
+        MarsApiStatus.DONE ->{
+            statusImageView.visibility = View.GONE
+        }
+        else -> {}
+    }
 }
